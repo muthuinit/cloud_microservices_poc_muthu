f64ec550c251ce435b3be966b7051c033ee613f3
muthuinit <muthuinit86@gmail.com>
2025-02-05 13:13:49 +0000

    updated use case files

--- deploy/app.py
+++ deploy/app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 import pickle
 import numpy as np
@@ -16,3 +17,9 @@ def home():
 def predict(size: float, bedrooms: int):
     prediction = model.predict(np.array([[size, bedrooms]]))
     return {"predicted_price": prediction[0]}
+
+# Explicitly listen on PORT 8080 for Cloud Run
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))  # Get PORT from env variable (default 8080)
+    uvicorn.run(app, host="0.0.0.0", port=port)
